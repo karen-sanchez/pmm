5ffc10a7d629c2287e62f1927e38173cb925c27f
karen sanchez <karen.s.sanchez08@gmail.com>
2016-09-28 18:40:52 -0400

    node-yelp-api

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-var express = require ('express');
+var express = require('express');
 var app = express();
-var config = require('./config')
 
-app.get('/', function(req,res){
-	res.send("Hello World!");
+// Request API access: http://www.yelp.com/developers/getting_started/api_access
+var Yelp = require('yelp');
+var config = require('./config');
+console.log(config);
+
+var secureYelp = new Yelp(config);
+
+// See http://www.yelp.com/developers/documentation/v2/search_api
+secureYelp.search({ term: 'food', location: 'New+York', limit: 1 })
+.then(function (data) {
+  console.log(data);
+})
+.catch(function (err) {
+  console.error(err);
 });
 
-app.listen(3000, function(){
-	console.log("All Right! That's Cool!");
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('All Right! Thats Cool');
 });
--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
 	consumer_key: process.env.yelp_consumer_key,
 	consumer_secret: process.env.yelp_consumer_secret,
 	token: process.env.yelp_token,
-	access_token_secret: process.env.yelp_token_secret
+	token_secret: process.env.yelp_token_secret
 }
