7a14cab2cf37dbd4d24d91a4187b2a4c8e1560d4
Christopher Mora <cmora09@gmail.com>
2016-09-28 18:17:06 -0400

    Added config module for keys and tokens

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	consumer_key: process.env.yelp_consumer_key,
+	consumer_secret: process.env.yelp_consumer_secret,
+	token: process.env.yelp_token,
+	access_token_secret: process.env.yelp_token_secret
+}
